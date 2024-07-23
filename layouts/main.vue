624d7feca496c818707197fa4db125f0dc1822e0
Peter Nohng <7.microsoft@gmail.com>
2024-07-23 12:51:30 +0700

    make it better

--- layouts/main.vue
+++ layouts/main.vue
@@ -3,9 +3,31 @@
 </script>
 <template>
     
-            <NuxtPage />
+    <div class="scrollbar-hidden">
+        <NuxtPage />
+    </div>
 
 </template>
 <style scoped>
+/* Apply this to hide scrollbars on all elements */
+.scrollbar-hidden {
+    -ms-overflow-style: none;  /* IE and Edge */
+    scrollbar-width: none;  /* Firefox */
+}
 
+/* WebKit and Blink (Chrome, Safari, newer versions of Opera) */
+.scrollbar-hidden::-webkit-scrollbar {
+    display: none;
+}
+
+/* Ensure the body also doesn't show scrollbars */
+body {
+    overflow: hidden;
+}
+
+/* Optional: If you want to allow scrolling on the main content */
+.scrollbar-hidden {
+    overflow-y: auto;
+    height: 100vh;
+}
 </style>
