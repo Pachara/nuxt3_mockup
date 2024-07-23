726fdb523a380962eb7f75d7973dbfd6e783320f
Peter Nohng <7.microsoft@gmail.com>
2024-07-23 12:09:37 +0700

    make it better

--- pages/index.vue
+++ pages/index.vue
@@ -58,7 +58,7 @@ onMounted(() => {
                         </div>
                         <div class="col-lg-3 col-6 mb-2 ps-2 pe-2 d-flex align-items-center justify-content-end order-2 order-lg-3 fw-normal">
                             <div class="contact-box-menu d-flex justify-content-end w-100 h-100 align-items-center">
-                               <button class="rounded-circle bg-warning text-dark me-1 d-flex justify-content-center align-items-center" style="width:40px;height:40px;">
+                               <button class="rounded-circle bg-success border-0 text-white me-1 d-flex justify-content-center align-items-center" style="width:40px;height:40px;">
                                 <i class="bi bi-telephone-fill fs-4"></i>
                                </button>
                                 <i class="bi bi-line text-success"  style="font-size:2.6em;margin-top:4px;"></i>
