c805ddfc195ae7f09b5dad6e732aea0aaff7f5f8
Peter Nohng <7.microsoft@gmail.com>
2024-07-24 14:16:09 +0700

    make it better

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,22 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
 
+  build: {
+    extractCSS: true,
+    optimizeCSS: true,
+  },
+
+  nitro: {
+    prerender: {
+      crawlLinks: true,
+      routes: ['/'],
+    },
+  },
+
+  experimental: {
+    payloadExtraction: true,
+  },
+
   css: [
     '@/assets/css/main.css',
     '@/assets/css/custom.css',
