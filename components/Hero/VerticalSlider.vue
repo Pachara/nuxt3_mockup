e73e66771ca35217b85cd6160844ecd8d6b7f9cb
Peter Nohng <7.microsoft@gmail.com>
2024-07-29 22:25:50 +0700

    make it better

--- components/Hero/VerticalSlider.vue
+++ components/Hero/VerticalSlider.vue
@@ -10,35 +10,39 @@ onMounted(() => {
     drag: 'free',
     focus: 'center',
     perPage: 3,
-    autoHeight: true,
+    perMove: 1,
     direction: 'ttb', // top-to-bottom for vertical
-    height: 'auto',
+    height: '360px', // Set a fixed height
     autoScroll: {
       speed: 1,
-      rewind: true, // Ensure it rewinds properly to create the bottom-to-top effect
+      pauseOnHover: false,
+      pauseOnFocus: false,
+      rewind: false,
     },
     arrows: false,
     pagination: false,
-    preloadPages: 1,
+    preloadPages: 3,
     throttle: 100,
+    gap: '1rem',
+    loop: true,
+    cloneStatus: true,
+    updateOnMove: true,
   });
 
   splideInstance.mount({ AutoScroll: $AutoScroll });
 });
 </script>
 
-
-
 <template>
-  <div class="w-100 ms-0 me-0 d-flex overflow-hidden order-md-2 order-1 ">
+  <div class="w-100 ms-0 me-0 d-flex overflow-hidden order-md-2 order-1">
     <div class="splide w-100 m-auto" ref="splide">
-      <div class="splide__track h-100" >
-        <ul class="splide__list h-100">
-          <li  v-for="(i,k) in 4" class="splide__slide " :key="k">
+      <div class="splide__track">
+        <ul class="splide__list">
+          <li v-for="(i,k) in 8" class="splide__slide" :key="k">
             <div class="box-container bg-success rounded-4 border border-3">
-              <div class="box-content  text-white ">
-                <nuxt-img src="https://randomuser.me/api/portraits/thumb/men/32.jpg"  class="rounded-circle m-1 border border-2" width="40px" height="40px" styl="float:left"/>
-               "ท่านสามารถจองรถยนต์ผ่านระบบออนไลน์แบบเรียลไทม์ โดยเลือกรุ่นรถและระบุวันที่ต้องการใช้งาน หรือหากต้องการความช่วยเหลือ "
+              <div class="box-content text-white">
+                <nuxt-img src="https://randomuser.me/api/portraits/thumb/men/32.jpg" class="rounded-circle m-1 border border-2" width="40px" height="40px" style="float:left"/>
+                "ท่านสามารถจองรถยนต์ผ่านระบบออนไลน์แบบเรียลไทม์ โดยเลือกรุ่นรถและระบุวันที่ต้องการใช้งาน หรือหากต้องการความช่วยเหลือ {{ i }}"
               </div>
             </div>
           </li>
@@ -48,10 +52,9 @@ onMounted(() => {
   </div>
 </template>
 
-
 <style scoped>
-.splide{
-  max-width:320px;
+.splide {
+  max-width: 320px;
 }
 .box-container {
   height: 120px;
@@ -60,23 +63,21 @@ onMounted(() => {
   display: flex;
   justify-content: center;
   align-items: center;
-  margin: 10px 0; /* Example margin */
+  margin: 10px 0;
 }
 
 .box-content {
   text-align: left;
-  padding: 20px; /* Example padding */
-  font-size: 16px; /* Example font size */
+  padding: 20px;
+  font-size: 16px;
 }
 
 .splide__track {
   height: 100%;
-  display: flex;
-  flex-direction: column-reverse; /* Reverse the direction */
 }
 
 .splide__list {
   display: flex;
-  flex-direction: column-reverse; /* Reverse the order of slides */
+  flex-direction: column;
 }
 </style>
