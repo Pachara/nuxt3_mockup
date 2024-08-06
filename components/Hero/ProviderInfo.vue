6c57ab589f5d2684efca7766c349bdd427eb5bc2
Peter Nohng <7.microsoft@gmail.com>
2024-08-06 11:02:01 +0700

    make it better

--- components/Hero/ProviderInfo.vue
+++ components/Hero/ProviderInfo.vue
@@ -1,6 +1,16 @@
+<script setup>
+  const config = useRuntimeConfig();
+
+</script>
 <template>
     <div class="rounded-3 w-100 p-3 position-relative">
-      <div class="pb-0">
+      <div class="pb-0 d-flex">
+        <div class="p-1 pe-3" >
+          <div class="border rounded-circle" style="width:150px;height:150px;">
+              <nuxt-img :src="`${config.public.baseURL}/images/profile/profile.png`" />
+          </div>
+        </div>
+        <div>
         <p class="d-flex align-items-center" style="margin:0;padding:0;font-size:0.9em;margin-bottom:3px;">
           <span class="badge rounded-pill text-bg-warning me-2 d-flex" style="font-size:0.6em;margin-left:-5px;border:1px solid black;padding-top:3px;padding-bottom:3px;">
             Premium Provider
@@ -18,15 +28,19 @@
             <span class="fs-5 ps-1 pe-1">1.5K Follows</span>
           </span>
         </p>
+        </div>
       </div>
+
       <p class="content-media fw-light m-0 pt-0">
         ศูนย์บริการรถเช่านครศรีธรรมราช มอบประสบการณ์ที่ประทับใจจนได้รับการบอกต่อมากที่สุดในจังหวัด เรามีจุดรับ-คืนรถทั้งที่สนามบินและในตัวเมืองเพื่อความสะดวกของลูกค้า สำหรับผู้ใช้บริการประจำ เรามีโปรแกรมส่วนลดพิเศษที่คุ้มค่า ด้วยมาตรฐานการบริการที่เป็นที่ยอมรับ จึงทำให้เราเป็นที่นิยมและได้รับการแนะนำอย่างกว้างขวางในนครศรีธรรมราช
       </p>
+      
+
+
     </div>
   </template>
   
-  <script setup>
-  </script>
+
   
   <style scoped>
   .h1-media {
